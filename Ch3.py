8b0a14f1a710ed2a7a0f91c13ce8a8dcb1b0d169
Eric Banavong <eb953@drexel.edu>
2023-02-02 14:49:23 -0800

    lists

--- Ch3.py
+++ Ch3.py
@@ -1,5 +1,23 @@
-# Lists 
+#Lists 
 
-bicyles = ['trek', 'cannondale', 'redline', 'specialized']
+# Index positions start at 0
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+print(bicycles[0].title())
+message = f"My first bicycle was a {bicycles[2].title()}"
+print(message)
 
+#try myself
+
+friends = ['shey', 'max', 'will', 'parker']
+print(friends)
+
+messageA = f"My friend {friends[0].title()} is from Guetamala"
+messageB = f"There is a friend named {friends[1].title()} who is from England"
+messageC = f"{friends[2].title()} was raised in Yorba Linda"
+messageD = f"A midwestern boy is something {friends[3].title()} would be considered as"
+
+print(messageA)
+print(messageB)
+print(messageC)
+print(messageD)
