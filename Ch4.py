8ae44f1c2208d86af9962d2d411384b29a6e53d5
Eric Banavong <eric.banavong@gmail.com>
2023-02-06 15:59:57 -0800

    loops

---
+++ Ch4.py
@@ -0,0 +1,16 @@
+## Loops 
+
+magicians = ['eric', 'max','will', 'parker']
+for x in magicians:
+    print(x)
+
+## x is the variable - better to use singluar and plural names -> magician in magicians 
+
+
+magicians = ['eric', 'max','will', 'parker']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!"
+    )
+
+
+
