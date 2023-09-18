62865ee48cc37f74fa205b6f0a84bb14c0e4abe4
Eric Banavong <eric.banavong@gmail.com>
2023-09-18 14:26:00 -0700

    new new

--- Ch5.py
+++ Ch5.py
@@ -123,4 +123,106 @@ if 'orange' in alien_color:
 else:
     print("The player has earned 10 points")
     
+#elif
 
+alien_color = ['green', 'yellow', 'red']
+
+if 'orange' in alien_color:
+    print("The player has earned 5 points for shooting the alien!")
+elif 'blue' in alien_color: 
+    print("The player has earned 10 points")
+elif 'red' in alien_color:
+    print("The player has earned 15 points")
+    
+#age
+
+age = 69
+if age < 2:
+    print("This person is a baby")
+elif age >= 2 and age < 4:
+    print("This person is a toddler")
+elif age >= 4 and age < 13:
+    print("This person is a kid")
+elif age >= 13 and age < 20:
+    print("This person is a teenager")
+elif age >= 20 and age < 65:
+    print("This person is an adult")
+elif age >= 65:
+    print("this person is an elder")
+
+#for loop:
+requested_topping = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_topping:
+    print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza")
+
+
+#how can we add an if statement:
+
+requested_topping = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_topping:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers")
+    else: 
+        print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza")
+
+#check empty list
+requested_toppings = ['cheese', 'sausage']
+
+if requested_toppings:
+    for requested_toppings in requested_toppings:
+        print(f"Adding {requested_toppings}")
+    print("\nFinished making your pizza")
+else:
+    print("are you sure you want a plain pizza?")
+    
+    
+available_topping = ['cheese', 'pepperoni', 'anchovies', 'olives', 'artichoke', 'sausage', 'pineapple']
+requested_topping = ['pepperoni', 'sausage', 'bbq']
+
+for requested_topping in requested_topping:
+    if requested_topping in available_topping:
+        print(f"Adding {requested_topping}")
+else:
+    print(f"Sorry, we don't have {requested_topping}")
+    
+print("\nFinished making your pizza")
+
+#practice:
+
+user = ['admin', 'jaden', 'eric', 'max', 'will']
+for user in user: 
+    if 'admin' in user:
+        print("Hello admin, would you like to see a status report")
+    else:
+        print(f"Hello {user}, thank you for logging in again")
+        
+
+#check users:
+current_users = ['Eric', 'Nathan','Brad','John']
+new_users = ['Eric', 'Max', 'Will']
+
+for new_users in new_users:
+    if new_users in current_users:
+        print(f"The username, {new_users}, is already being used")
+    else:
+        print(f"{new_users}, is an available username")
+        
+print("\nFinished making your username")
+
+
+#ordinal numbers
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+for numbers in numbers: 
+    if '1' in numbers:
+        print(f"{numbers}st")
+    elif '2' in numbers:
+        print(f"{numbers}nd")
+    elif '3' in numbers:
+        print(f"{numbers}rd")
+    else:
+        print(f"{numbers}th")
+        
