1fbd5d4d0dfc4017d75834918fc9c82994a9bc04
Eric Banavong <eric.banavong@gmail.com>
2023-11-04 22:57:35 -0700

    new

--- ch9.py
+++ ch9.py
@@ -195,3 +195,36 @@ class Car:
 my_new_car = Car('auid', 'a4', 2019)
 my_new_car.update_odometer(25) 
 my_new_car.read_odometer()
+
+##add logic to method update_odometer() to do additional work every time the odometer reading is modified 
+class Car: 
+    """Represent a car"""
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year  
+        self.odometer_reading = 0 
+    
+    def get_descriptive_name(self):
+        """return a formatted descriptive name"""
+        long_name = f"{self.make} {self.model} {self.year}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """print a statement showing the car's mileage"""
+        print(f"This car has {self.odometer_reading} miles on it")
+    
+    def update_odometer(self, mileage): ## adding method to update certain attributes 
+        """Set the odometer reading to the given value. Reject the change if it attempts to roll the odometer back"""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage 
+        else:
+            print("You can't roll back an odometer")
+            
+my_new_car = Car('auid', 'a4', 2019)
+my_new_car.update_odometer(20) 
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(15)
+my_new_car.update_odometer(30)
+my_new_car.read_odometer()
