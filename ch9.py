a125995d80b6caa21db66efe8649d74b6962032d
Eric Banavong <eric.banavong@gmail.com>
2023-11-04 22:52:48 -0700

    new

--- ch9.py
+++ ch9.py
@@ -169,4 +169,29 @@ class Car:
 my_new_car = Car('auid', 'a4', 2019)
 my_new_car.odometer_reading = 23 ## adding direct value
 my_new_car.read_odometer()
+
+##modifying an attribute's value through a method 
+
+class Car: 
+    """Represent a car"""
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year  
+        self.odometer_reading = 0 
     
+    def get_descriptive_name(self):
+        """return a formatted descriptive name"""
+        long_name = f"{self.make} {self.model} {self.year}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """print a statement showing the car's mileage"""
+        print(f"This car has {self.odometer_reading} miles on it")
+    
+    def update_odometer(self, mileage): ## adding method to update certain attributes 
+        self.odometer_reading = mileage
+        
+my_new_car = Car('auid', 'a4', 2019)
+my_new_car.update_odometer(25) 
+my_new_car.read_odometer()
