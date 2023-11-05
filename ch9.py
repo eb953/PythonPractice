826c61178815f244c468cbec02e5f0756330614e
Eric Banavong <eric.banavong@gmail.com>
2023-11-04 23:30:47 -0700

    new

--- ch9.py
+++ ch9.py
@@ -228,3 +228,42 @@ my_new_car.read_odometer()
 my_new_car.update_odometer(15)
 my_new_car.update_odometer(30)
 my_new_car.read_odometer()
+
+##Incrementing an attribute's value through a method 
+class Car: 
+    """Represent a car"""
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year  
+        self.odometer_reading = 0 
+    
+    def get_descriptive_name(self):
+        """return a formatted descriptive name"""
+        long_name = f"{self.make} {self.model} {self.year}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """print a statement showing the car's mileage"""
+        print(f"This car has {self.odometer_reading} miles on it")
+        
+    def update_odometer(self, mileage): ## adding method to update certain attributes 
+        """Set the odometer reading to the given value. Reject the change if it attempts to roll the odometer back"""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage 
+        else:
+            print("You can't roll back an odometer")
+    
+    def increment_odometer(self, miles):
+        """add the given amount to the odometer reading"""
+        self.odometer_reading += miles
+            
+my_used_car = Car('auid', 'a4', 2019)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
