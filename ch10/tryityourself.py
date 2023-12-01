758b4b00d99b686b18c9b066fca7d866215a40ba
Eric Banavong <eric.banavong@gmail.com>
2023-11-30 19:17:25 -0800

    new

--- ch10/tryityourself.py
+++ ch10/tryityourself.py
@@ -14,3 +14,21 @@ while True:
         print("One of the values inputed is not a number")
     print(answer)
     
+
+## add pass 
+
+print("give me two numbers, and I'll add them")
+print("Enter 'q' to quit")
+
+while True:
+    first_number = input("What is the first number: ")
+    if first_number == 'q':
+        break 
+    second_number = input("What is the second number: ")
+    if second_number == 'q': 
+        break 
+    try:
+        answer = int(first_number) + int(second_number)
+    except ValueError:
+        pass
+    print(answer)
