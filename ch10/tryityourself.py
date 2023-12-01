207618086a97f91037d5e8010cdd170daf5bb80c
Eric Banavong <eric.banavong@gmail.com>
2023-12-01 15:18:58 -0800

    new

--- ch10/tryityourself.py
+++ ch10/tryityourself.py
@@ -71,3 +71,32 @@ for filename in filenames:
     except FileNotFoundError:
         print("  Sorry, I can't find that file.")
 
+##10-9 Silent cats and dogs 
+
+filenames = ['cats.txt', 'dogs.txt', 'birds.txt']
+
+for filename in filenames:
+    print(f"\nReading file: {filename}")
+    try:
+        with open(filename) as f:
+            contents = f.read()
+            print(contents)
+    except FileNotFoundError:
+        pass
+
+##10-10 common words 
+
+def countWords(filename):
+    """count words"""
+    try:
+        with open (filename, encoding='utf-8') as f:
+            line = f.read()
+    except FileNotFoundError:
+        pass
+    else:
+       num_line = line.lower().count('the')
+       print(num_line)
+
+filename = 'alice.txt',
+countWords(filename)
+        
