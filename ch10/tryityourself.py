7d79d9c6ef041f4ec9a1413f2e862dfcc66a262d
Eric Banavong <eric.banavong@gmail.com>
2023-11-30 19:07:14 -0800

    new

---
+++ ch10/tryityourself.py
@@ -0,0 +1,16 @@
+print("give me two numbers, and I'll add them")
+print("Enter 'q' to quit")
+
+while True:
+    first_number = input("What is the first number: ")
+    if first_number == 'q':
+        break 
+    second_number = input("What is the second number: ")
+    if second_number == 'q': 
+        break 
+    try:
+        answer = int(first_number) + int(second_number)
+    except ValueError:
+        print("One of the values inputed is not a number")
+    print(answer)
+    
--- ch10/word_count.py
+++ ch10/word_count.py
@@ -14,4 +14,23 @@ filenames = ['alice.txt','pattern.txt','russian.txt']
 for filename in filenames:
    count_words(filename)
 
+##Failing Silently 
+# You can add pass in in the try else block and Python will continue running despite errors 
+
+
+def count_words(filename):
+    """Count the approximate number of words in a file"""
+    try:
+        with open(filename, encoding='utf-8') as f:
+         contents = f.read()
+    except FileNotFoundError:
+        pass ## will continue running despite file not found 
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words")
+
+filenames = ['alice.txt','pattern.txt','russian.txt', 'adobe.txt']
+for filename in filenames:
+   count_words(filename)
     
