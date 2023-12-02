90c112807140c979e49eca919b09cc8313523980
Eric Banavong <eric.banavong@gmail.com>
2023-12-01 16:12:31 -0800

    new

--- ch10/remember_me.py
+++ ch10/remember_me.py
@@ -25,3 +25,37 @@ except FileNotFoundError:
         print(f"We'll remember you when you come back, {username}")
 else:
     print(f"welcome back, {username}")
+
+
+##Refactoring: process of improving code by breaking it up into a series of functions that have a specific job 
+
+import json
+def get_stored_username():
+    """Get stored username if available """
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+    
+def get_new_username():
+    """prompt for a new username"""
+    username = input("What is your name")
+    filename = 'username.json'
+    with open(filename, 'w') as f: 
+        json.dump(username, f)
+    return username
+
+def greetUser(): 
+    """greet the user by name"""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}")
+get_new_username() 
+get_stored_username()
