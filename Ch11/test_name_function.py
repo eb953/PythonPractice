d29f8481468a7854545deba85475bd405e011b36
Eric Banavong <eric.banavong@gmail.com>
2023-12-05 14:51:45 -0800

    new

--- Ch11/test_name_function.py
+++ Ch11/test_name_function.py
@@ -9,9 +9,14 @@ class NamesTestCase(unittest.TestCase):
     """Tests for 'name_function.py'"""
 
     def test_first_last_name(self):
-        """do names like 'janis joplin work?"""
+        """do names like 'janis sophia joplin work?"""
         formatted_name = get_formatted_name('janis','joplin')
         self.assertEqual(formatted_name, 'Janis Joplin') #assert method verify that a result you received matches the reult you expect to receive 
+
+    def test_first_last_middle(self):
+        """do names with middle name work"""
+        formatted_name = get_formatted_name('Eric','Banavong', 'Robert')
+        self.assertEqual(formatted_name, 'Eric Robert Banavong')
 
 if __name__ == '__main__':
     unittest.main() 
