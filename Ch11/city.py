70c1f2744edaa8ba3e812dd729154ff57737fbd2
Eric Banavong <eric.banavong@gmail.com>
2023-12-05 16:17:52 -0800

    new

--- Ch11/city.py
+++ Ch11/city.py
@@ -5,9 +5,10 @@ while True:
     city = input("What is the city name?")
     if city == 'q':
         break
-    country = input("What is the location of the cityu?")
+    country = input("What is the location of the city?")
     if country == 'q':
         break 
+    population = input("What is the population?")
 
-    formatted_city = get_city_name(city, country)
+    formatted_city = get_city_name(city, country, population)
     print(f"{formatted_city}")
