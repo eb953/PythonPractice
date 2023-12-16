04fed6d19e3820ae9d22ce463fa5b17245524659
Eric Banavong <eric.banavong@gmail.com>
2023-12-15 16:43:21 -0800

    new

--- Ch11/testEmployee.py
+++ Ch11/testEmployee.py
@@ -1,4 +1,15 @@
 import unittest
 from employee import EmployeeInfo
 
+class testEmployeeInfo(unittest.TestCase):
+
+    def setUp(self):
+        self.eric=('Eric','Banavong', 200000)
+
+    def test_give_default_raise(self):
+        self.eric.give_raise()
+        self.assertEqual(self.eric.salary, 70000)
+unittest.main()
+    
+
 
